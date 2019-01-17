4939ed40514b6565f218d301a9e981ece34da7a5
Alex Otano <alexis.otano@otecno.com>
2019-01-17 12:31:59 -0600

    .

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 var url = window.location.href;
 var swLocation = '/twiitor/sw.js';
-
+console.log('URL:', url)
 if( navigator.serviceWorker ) {
     if ( url.includes( 'localhost' ) ){
-        swLocation = 'sw.js';
+        swLocation = '/sw.js';
     }
 
     navigator.serviceWorker.register(swLocation);
